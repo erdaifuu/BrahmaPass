d2cc49b9dea604e06528f339a08fc145f4c962d6
mrmcrnr <ejkim360@gmail.com>
2023-02-27 12:29:39 -0800

    Eddie 2/27/2023

--- app/src/main/java/com/example/brahmapassv3/MainActivity.kt
+++ app/src/main/java/com/example/brahmapassv3/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.brahmapassv3.scnlogin.ScnLogin
 import com.example.brahmapassv3.ui.theme.BrahmapassV3Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +19,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             BrahmapassV3Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
             }
         }
     }
@@ -31,10 +29,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BrahmapassV3Theme {
-        Greeting("Android")
+        ScnLogin()
     }
-}
+}*/
