ce387bb4bd3c08839a2364bbf8fa08d1708d817f
mrmcrnr <ejkim360@gmail.com>
2023-03-10 12:18:10 -0800

    Eddie 3/10/2023

--- app/src/main/java/com/example/brahmapassv3/MainActivity.kt
+++ app/src/main/java/com/example/brahmapassv3/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.brahmapassv3
+/*package com.example.brahmapassv3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,32 +7,59 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.brahmapassv3.scnhome.ScnHome
 import com.example.brahmapassv3.scnlogin.ScnLogin
 import com.example.brahmapassv3.ui.theme.BrahmapassV3Theme
+import androidx.window.*
+import java.util.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BrahmapassV3Theme {
-                // A surface container using the 'background' color from the theme
+                //val WindowSizeClass = calculateWindowSizeClass(this)
             }
         }
+       // computeWindowSizeClasses()
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-/*@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BrahmapassV3Theme {
-        ScnLogin()
+    /*private fun computeWindowSizeClasses() {
+        val metrics = WindowMetricsCalculator.getOrCreate()
+            .computeCurrentWindowMetrics(this)
+        val widthDp = metrics.bounds.width() /
+                resources.displayMetrics.density
+        val widthWindowSizeClass = when {
+            widthDp < 600f -> WindowSizeClass.COMPACT
+            widthDp < 840f -> WindowSizeClass.MEDIUM
+            else -> WindowSizeClass.EXPANDED
+        }
+        val heightDp = metrics.bounds.height() /
+                resources.displayMetrics.density
+        val heightWindowSizeClass = when {
+            heightDp < 480f -> WindowSizeClass.COMPACT
+            heightDp < 900f -> WindowSizeClass.MEDIUM
+            else -> WindowSizeClass.EXPANDED
+        }
+    }*/
     }
-}*/
+
+
+    @Composable
+    fun HomeScreen(name: String) {
+        //val navController = rememberNavController()
+        ScnHome(
+            txtEnterIDOnHomeTextContent = "Enter ID... ",
+            txtEnterReasonOnHomeTextContent = "Enter here... "
+        )
+    }
+
+    fun getCurrentDateTime(): Date {
+        return Calendar.getInstance(TimeZone.getTimeZone("PST")).time
+    }
+
+*/
